23d1aa95bac0e0082921388e03a6468e6f4b81ee
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-05 19:42:25 -0700

    created an item class and added a room as well as 10 items.

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,6 @@
 from room import Room
 from player import Player
-import argparse
-import sys
-
-# Construct and parse parameters
-ap = argparse.ArgumentParser()
+from item import Item
 
 # Declare all the rooms
 
@@ -13,7 +9,7 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east. The door outside is now locked."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -25,21 +21,51 @@ to north. The smell of gold permeates the air."""),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+    'incinerator': Room("Incinerator Room", """I believe this place has ill-purpose. 
+    you see a trap-door in the ceiling.""")
 }
 # for n in room:
 #     print(room[n])
 
+items = {
+    'candelabra': Item("candelabra", """The wax has dried out and it's not useful 
+    as a light source."""),
+
+    'snow_globe': Item("snow globe", """There appears to be water and small white 
+    flakes, but there are palm trees on a beach as well."""),
+
+    'dagger': Item("dagger", """It's rather dull."""),
+
+    'broken_compas': Item("broken compass", """You wouldn't really need this in this 
+    place anyway."""),
+
+    'wirts_leg': Item("Wirt's leg", """Hmm, where is that tome of town portal?"""),
+
+    'cell_phone': Item("cell phone", """No reception, it looks like it wants me to enter 
+    a number."""),
+
+    'teddy_bear': Item("Sir Stuffs-a-lot", """How do I know that?"""),
+
+    'time_piece': Item("Trollex", """a cheap knock-off"""),
+
+    'lucky_cat': Item("lucky neko", """This one has its left paw raised"""),
+
+    'gold_coin': Item("Inca Gold", """This looks valuable, I hope I'm not asked to 
+    destroy it."""),
+}
 
 # Link rooms together
 
 room['outside'].n_to = room['foyer'] # has attribute .n_to
-room['foyer'].s_to = room['outside']
+# room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['incinerator'].u_to = room['outside']
 
 #
 # Main
---
+++ src/item.py
@@ -0,0 +1,4 @@
+class Item():
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
